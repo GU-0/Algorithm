a6a5261b8bb080597b8dd6e43519f0ecbe13e851
GU-0 <rjsgud5386@gmail.com>
2024-03-22 13:51:18 +0900

    programmers lv2 카펫

--- math/lv2_카펫.py
+++ math/lv2_카펫.py
@@ -1,19 +1,3 @@
-import numpy as np
-
-
-def solution(brown, yellow):
-    answer = []
-    x_add_y = brown / 2 + 2
-    x_mul_y = brown + yellow
-
-    x_sub_y = np.sqrt(np.power(x_add_y, 2) - 4 * x_mul_y)
-
-    x = (x_add_y + x_sub_y) / 2
-    y = (x_add_y - x_sub_y) / 2
-
-    return [x, y]
-
-
 """
 전체 카펫의 가로를 x, 세로를 y라고 한다면
 
@@ -28,9 +12,19 @@ xy = {brown} + {yellow}
 (x+y)^2 - 4xy = (x-y)^2
 
 (x-y)^2 = ({brown}^2 / 4) + 2{brown} + 4 - 4{brown} - 4{yellow}
-
-
-
 """
 
-print(solution(10, 2))
+import numpy as np
+
+
+def solution(brown, yellow):
+    answer = []
+    x_add_y = brown / 2 + 2
+    x_mul_y = brown + yellow
+
+    x_sub_y = np.sqrt(np.power(x_add_y, 2) - 4 * x_mul_y)
+
+    x = (x_add_y + x_sub_y) // 2
+    y = (x_add_y - x_sub_y) // 2
+
+    return [x, y]
