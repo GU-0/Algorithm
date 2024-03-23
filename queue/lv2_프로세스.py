0f9fff05f3a80b43b9396afdeea17749083e98c2
GU-0 <rjsgud5386@gmail.com>
2024-03-23 14:24:38 +0900

    programmers lv2 프로세스

--- queue/lv2_프로세스.py
+++ queue/lv2_프로세스.py
@@ -2,16 +2,16 @@ from collections import deque
 
 
 def solution(priorities, location):
-    queue = deque([(i, p) for i, p in enumerate(priorities)])
-    count = 0
+    queue = deque([(i, j) for i, j in enumerate(priorities)])  # [index, priority]
 
+    cnt = 0
     while queue:
-        # 현재 문서의 인덱스와 중요도
-        current = queue.popleft()
-        # 나머지 문서 중에 현재 문서보다 중요도가 높은 문서가 하나라도 있다면
-        if any(current[1] < other[1] for other in queue):
-            queue.append(current)  # 현재 문서를 맨 뒤로 보낸다
+        a, b = queue.popleft()
+        if any(b < item[1] for item in queue):
+            queue.append((a, b))
         else:
-            count += 1  # 인쇄한다
-            if current[0] == location:  # 요청한 문서라면 함수 종료
-                return count
+            cnt += 1
+            if a == location:
+                return cnt
+
+    return cnt
